152e8896811c6a85d6b873201f5b7c079c8568c7
Vadim354 <yus_vadim@mail.ru>
2025-05-18 16:55:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask("Магазин подарков", template_folder="", static_folder
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
 
 
 
