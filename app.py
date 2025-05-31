e59b2e99056bd9980e0f06feb97a22e06dedf035
Vadim354 <yus_vadim@mail.ru>
2025-05-31 18:20:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask("Магазин подарков", template_folder="", static_folder="")
+app.secret_key = '1234'
 
+products = {
+    1:{
+        'name':
+        'price':
+        'image:
+    },
+    2:{
+        'name':
+        'price':
+        'image:
+    }
+}
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", products=products)
 @app.route('/cart')
 def cart():
     return render_template("cart.html")
